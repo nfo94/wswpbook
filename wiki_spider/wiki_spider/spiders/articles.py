b2324ab9c2fabf535dd42f3d17af97b3af2429ea
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-01-31 21:47:23 -0300

    add chapter 05 [wip]

--- wiki_spider/wiki_spider/spiders/articles.py
+++ wiki_spider/wiki_spider/spiders/articles.py
@@ -1,5 +1,5 @@
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
 class ArticleSpider(CrawlSpider):
@@ -13,3 +13,9 @@ class ArticleSpider(CrawlSpider):
         title = response.css("h1::text").extract_first()
         text = response.xpath('//div[@id="mw-content-text"]//text()').extract()
         lastUpdated = response.css("li#footer-info-lastmod::text").extract_first()
+        lastUpdated = lastUpdated.replace("This page was last edited on ", "")
+
+        print("URL is: {}".format(url))
+        print("title is: {} ".format(title))
+        print("text is: {}".format(text))
+        print("Last updated: {}".format(lastUpdated))
